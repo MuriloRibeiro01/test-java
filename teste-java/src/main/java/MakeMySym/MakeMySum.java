652d50a823e06696c80791b0b8202920329bdb14
MURILO RIBEIRO DA SILVEIRA <murilo.silveira@a.ucb.br>
2026-03-25 11:04:16 -0300

    initial commit

---
+++ teste-java/src/main/java/MakeMySym/MakeMySum.java
@@ -0,0 +1,11 @@
+package MakeMySym;
+
+import java.util.Scanner;
+
+public class MakeMySum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Oxe tropa");
+    }
+}
---
+++ teste-java/src/main/java/MakeMySym/Soma.java
@@ -0,0 +1,12 @@
+package MakeMySym;
+
+import java.util.Scanner;
+
+public class Soma {
+    Scanner scanner = new Scanner(System.in);
+    int num1 = scanner.nextInt();
+    int num2 = scanner.nextInt();
+    int sum = num1 + num2;
+
+    public void 
+}
