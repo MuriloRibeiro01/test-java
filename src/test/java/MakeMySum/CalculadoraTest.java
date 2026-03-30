c9c92954ca2f89f39786aebfee6443f999040e4f
Murilo Ribeiro da Silveira <murilo.ribeiro2709@gmail.com>
2026-03-29 23:31:26 -0300

    Mudanças na estrutura de pastas, para que testes passem.

--- src/test/java/MakeMySum/CalculadoraTest.java
+++ src/test/java/MakeMySum/CalculadoraTest.java
@@ -1,14 +1,13 @@
 package MakeMySum;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculadoraTest {
     @Test
     public void testSoma() {
         Calculadora calc = new Calculadora();
 
-        assertEquals(5, calc.soma(1, 2));
+        assertEquals(3, calc.soma(1, 2));
     }
 }
